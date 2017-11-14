84a9f37c7a46070f0e87a931dde736313dfcc8c0
Henry Post <HenryFBP@gmail.com>
2017-11-13 22:33:28 -0600

    finished pp5

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,27 @@
+var doorbellpath = "media/doorbell.mp3";
+var currentlyPlaying;
+
+
 function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
+  var doorbell = new Audio(doorbellpath);
   doorbell.play();
 }
+
+$(document).ready(function(){
+  $('audio#doorbell').replaceWith("<button id=\"doorbell\">Ring the Doorbell</button>");
+
+
+  $('#doorbell').click(function()
+  {
+    console.log("currentlyPlaying is this: "+currentlyPlaying)
+    if(currentlyPlaying == null ||
+      currentlyPlaying == undefined ||
+      currentlyPlaying.ended === true) //if garbage collected, allow playing.
+    {
+      currentlyPlaying = new Audio(doorbellpath);
+      currentlyPlaying.play();
+
+    }
+  })
+
+})
